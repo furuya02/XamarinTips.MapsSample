03146d58449923ad7e1d801a7609a9a791078614
SIN <sin@sapporoworks.ne.jp>
2015-03-15 15:34:40 +0900

    sequel to the Maps

--- MapsSample/MapsSample/App.cs
+++ MapsSample/MapsSample/App.cs
@@ -1,45 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 
-namespace MapsSample {
-    public class App : Application {
-        public App() {
+namespace MapsSample
+{
+    public class App : Application
+    {
+        public App()
+        {
             MainPage = new MyPage();
+            
         }
 
-        protected override void OnStart() {
+        protected override void OnStart()
+        {
             // Handle when your app starts
         }
 
-        protected override void OnSleep() {
+        protected override void OnSleep()
+        {
             // Handle when your app sleeps
         }
 
-        protected override void OnResume() {
+        protected override void OnResume()
+        {
             // Handle when your app resumes
         }
     }
-
     public class MyPage : ContentPage
     {
-        public MyPage() {
-            var x = MapSpan.FromCenterAndRadius(new Position(35.71, 139.81), Distance.FromMiles(0.2));
+        public MyPage(){
+            var map = new Map(               
+              MapSpan.FromCenterAndRadius(   
+                new Position(35.71, 139.81), 
+                Distance.FromMiles(0.2))){  
+                    IsShowingUser = true, // 現在地の表示／非表示
+                    MapType = MapType.Street,//タイプ指定(Street/Satellite/Hybrid)
+                    VerticalOptions = LayoutOptions.FillAndExpand
+                };
 
-            var map = new Map(
-                MapSpan.FromCenterAndRadius(new Position(35.71, 139.81), Distance.FromMiles(0.2))){
-                            IsShowingUser = true,//現在位置の表示
-                            //HeightRequest = 100,
-                            //WidthRequest = 960,
-                            VerticalOptions = LayoutOptions.FillAndExpand
-                        };
-            Content = new StackLayout {
-                Children = { map }
+            //ピン表示のサンプル
+            var pin = new Pin{
+                Type = PinType.Place,//ピンの種類(Place以外を指定しても、特に変化が無い。　使用例も見つけられない)
+                Position = new Position(35.71, 139.81), // 座標は、同じくスカイスリー
+                Label = "東京スカイツリー",
+                Address = "〒131-0045 東京都墨田区押上１丁目１−２"
+            };
+            map.Pins.Add(pin);
+            
+            //スライダーによる拡大縮小のサンプル
+            var slider = new Slider(1, 18, 1);
+            slider.ValueChanged += (s, e) =>{
+                var zoomLevel = e.NewValue;
+                var latlongdegrees = 360 / (Math.Pow(2, zoomLevel));
+                map.MoveToRegion(new MapSpan(map.VisibleRegion.Center, latlongdegrees, latlongdegrees));
+            };
+
+            Content = new StackLayout{
+                //Children = { map }
+                Children = { map,slider }
             };
         }
+
     }
 }
